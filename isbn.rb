c9691fa59965481ce3964959aed0c7c8a8886e4d
Josh Gwinn <jdgwinn911@gmail.com>
2018-08-09 11:47:57 -0400

    passed for making sure that isbn10 number is correct

--- isbn.rb
+++ isbn.rb
@@ -1,5 +1,24 @@
 def isbn_10(num)
     num.gsub!(/[- ]/, '')
+    wrong = 0 
+    if num.gsub(/[\D]/, '') != num
+        wrong += 1
+    end
+    if num.end_with?('x') || num.end_with?('X')
+     wrong -= 1
+     check = num.gsub(/[\D]/, '')
+     x = num[-1]
+    if "#{check}#{x}" != num
+          wrong += 1
+        end
+     end
+
+    unless wrong == 0
+        return false
+    end
+
+
+
     num
 end
    
